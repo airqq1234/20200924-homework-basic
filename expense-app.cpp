d94b43da4b92384d50aee3e66ea1ea68aa96ff4d
lin charlie <a755405@stdmail.nssh.ntpc.edu.tw>
2020-09-27 15:23:06 +0800

    homework

---
+++ expense-app.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+  int num;
+  int amount = 0;
+  int count;
+  cin >> count;
+  for (int i = 0; i < count; i++) {
+    cin >> num;
+    amount = amount + num;
+  }
+  cout << amount;
+}
+
